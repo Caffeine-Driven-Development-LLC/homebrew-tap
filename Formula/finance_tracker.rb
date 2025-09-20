5ec558e842cb825745627e2b5943acb9685c5c09
Ian McNaughton <ianmcnaugh@gmail.com>
2025-09-20 14:01:13 -0500

    Update finance_tracker.rb

--- Formula/finance_tracker.rb
+++ Formula/finance_tracker.rb
@@ -1,12 +1,12 @@
 class FinanceTracker < Formula
   desc "Personal finance tracking CLI"
   homepage "https://github.com/Caffeine-Driven-Development-LLC/finance_tracker"
-  version "0.1.3"
+  version "0.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Caffeine-Driven-Development-LLC/finance_tracker/releases/download/v0.1.3/finance_tracker-v0.1.3-darwin-arm64.tar.gz"
-      sha256 "bb1965de7772141a96cf9f3b6264122afddc1123b35dd8a304aa5d2245a0e985"
+      url "https://github.com/Caffeine-Driven-Development-LLC/finance_tracker/releases/download/v0.2.0/finance_tracker-v0.2.0-darwin-arm64.tar.gz"
+      sha256 "e0b2e10e642e5ec7f11825ae20817a7a57d9eba3c88fcb574b4f525e1561c4c1"
     end
   end
 
